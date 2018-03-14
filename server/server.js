d200b837082fe5d8d5dc4931602a55a44ed77c29
= <qais.gh11@gmail.com>
2018-03-13 20:51:54 -0500

    WIP building the server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,13 @@
 import express from 'express';
-
+import bodyParser from 'body-parser';
 const app = express();
+app.use(bodyParser());
+
+app.post('/', (req , res, next) => {
+  console.log(req.body)
+  res.send('hello world')
+  next()
+})
 
 const server = app.listen(3000, () => {
   const { address, port } = server.address();
