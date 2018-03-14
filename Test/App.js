b5b88fdf4c644db0e1386852eff785ec6dc0926f
= <qais.gh11@gmail.com>
2018-03-13 20:50:43 -0500

    [WIP push notifications] getting the token from gcm and post it to the server

--- Test/App.js
+++ Test/App.js
@@ -8,6 +8,46 @@ import {
 import  {StackNavigator} from 'react-navigation';
 import Login from './components/login/Login';
 import Welcome from './components/welcome/Welcome';
+import PushNotification from 'react-native-push-notification'
+// const token = {
+//   token: null
+// }
+PushNotification.configure({
+
+    // (optional) Called when Token is generated (iOS and Android)
+    onRegister: function(token) {
+        console.log( 'TOKEN:', token );
+        // token.token = token;
+        fetch('http://10.0.2.2:3000/', {
+  method: 'POST',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(token)
+})
+    },
+
+    onNotification: function(notification) {
+        setTimeout(() => {
+          if(!notification['foreground']) {
+            ToastAndroid.show('you have clicked', ToastAndroid.SHORT)
+          }
+        }, 1)
+       
+        PushNotification.localNotificationSchedule({
+          title: "this is the title",
+          message: notification['name'], // (required)
+          date: new Date(Date.now())
+        });
+    },
+    senderID: "1096237688973",
+
+});
+
+
+
+
 
 const RootStack = StackNavigator(
   {
--- Test/components/welcome/Welcome.js
+++ Test/components/welcome/Welcome.js
@@ -19,6 +19,7 @@ export default class Welcome extends Component {
 
   componentDidMount() {
      this.GetTime();
+     console.log('اي شي')
   }
   	
   GetTime() {
