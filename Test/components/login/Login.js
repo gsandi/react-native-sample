31cbc4ea6e96f3eac37b46c5f9577466c3c10a64
= <qais.gh11@gmail.com>
2018-03-14 21:57:33 -0500

    finished login component clean up

--- Test/components/login/Login.js
+++ Test/components/login/Login.js
@@ -1,52 +1,40 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {View} from 'react-native';
-import Button from 'react-native-button';
 import HeaderText from '../header-text/header-text'
-import {StackNavigator} from 'react-navigation';
+import Button from 'react-native-button';
 import {GoogleSignin} from 'react-native-google-signin';
+import {googleConfig} from './google-config'
 import styles from '../../style/style';
 
-export default class Login extends Component {
+export default class Login extends React.Component {
 
-  componentDidMount() {
-    GoogleSignin.hasPlayServices({ autoResolve: true })
-    .then(() => {})
-    .catch((err) => {
+  componentWillMount() {
+    GoogleSignin.hasPlayServices({ autoResolve: true }).catch( err => {
       console.log("Play services error", err.code, err.message);
     });
-
-    GoogleSignin.configure({
-      scopes: ["https://www.googleapis.com/auth/drive.readonly"], 
-      webClientId: "408004852008-6bcqgg84o620qdcnoti93ba3hmuij8dh.apps.googleusercontent.com", 
-    })
-    }
-
-  handleSignin() {
-    GoogleSignin.signIn()
-    .then((user) => {
-      this.setState({user: user});
-      this.props.navigation.navigate('Welcome' , {name: this.state.user.givenName, photo: this.state.user.photo });
-    })
-    .catch((err) => {
-      console.log('WRONG SIGNIN', err);
-    })
-    .done();
+    
+    GoogleSignin.configure(googleConfig)
   }
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      user:  {}
-    };
+  handleSignin() {
+    GoogleSignin.signIn().then( user => {
+      this.setState({user: user});
+
+      this.props.navigation.navigate('Welcome' , {
+        name: this.state.user.givenName, 
+        photo: this.state.user.photo 
+      });
+
+    }).catch( err => {
+      console.log('WRONG SIGNIN', err);
+    }).done();
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <HeaderText text='Hi There!! Please '/>
-        <Button
-          style={styles.signInButton}
-          onPress={this.handleSignin.bind(this)}>
+        <HeaderText text='Hi There!! Please'/>
+        <Button style={styles.signInButton} onPress={this.handleSignin.bind(this)}>
           Sign in with Google
         </Button>
       </View>
