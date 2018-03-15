ef5d27f3829137b1d0eb08cd500949e791607a59
= <qais.gh11@gmail.com>
2018-03-14 21:27:38 -0500

    clean up the code

--- Test/components/welcome/Welcome.js
+++ Test/components/welcome/Welcome.js
@@ -1,40 +1,27 @@
-import React, { Component } from 'react';
-import {
-  Text,
-  View,
-  Image
-} from 'react-native';
+import React from 'react';
+import {View, Image} from 'react-native';
 import Button from 'react-native-button';
 import HeaderText from '../header-text/header-text';
 import {GoogleSignin} from 'react-native-google-signin';
-import  {StackNavigator} from 'react-navigation';
 import {pushNotification} from '../../services/push-notification.service.js';
 import styles from '../../style/style';
 
-pushNotification.init()
-
-export default class Welcome extends Component {
-  constructor(props) {
-    super(props);
+export default class Welcome extends React.Component {
+  constructor() {
+    super();
     this.state = {
       time: ''
     };
   }
 
-  componentDidMount() {
-     this.GetTime();
+  componentWillMount() {
+    pushNotification.init()
+    this.setGreetingsMessage();
   }
   	
-  GetTime() {
-    var date, hour, greetings;
-    date = new Date();
-    hour = date.getHours(); 
-    if(hour <= 11){
-      greetings = 'Good Morning'
-    }
-    else{
-      greetings = 'Good Evening'
-    }
+  setGreetingsMessage() {
+    const hour = new Date().getHours();
+    const greetings = (hour <= 11) ? 'Good Morning' : 'Good Evening'
     this.setState({
       time: greetings
     });
