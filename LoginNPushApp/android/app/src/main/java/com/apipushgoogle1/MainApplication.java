ca44e48b628c9b5f974293a308c3ebe28a0b9784
Ruchi Goyal <goyal.r@husky.neu.edu>
2018-03-19 22:47:24 -0700

    Orientation and push notification support

--- LoginNPushApp/android/app/src/main/java/com/apipushgoogle1/MainApplication.java
+++ LoginNPushApp/android/app/src/main/java/com/apipushgoogle1/MainApplication.java
@@ -3,6 +3,8 @@ package com.apipushgoogle1;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.github.yamill.orientation.OrientationPackage;
+import com.reactlibrary.googlesignin.RNGoogleSignInPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -10,6 +12,8 @@ import com.facebook.soloader.SoLoader;
 
 import java.util.Arrays;
 import java.util.List;
+
+import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -22,7 +26,10 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+      new MainReactPackage(),
+      new OrientationPackage(),
+      new RNGoogleSignInPackage(),
+      new ReactNativeOneSignalPackage()
       );
     }
 
