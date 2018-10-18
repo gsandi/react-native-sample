ce4a445c38e00bf629721eb722d1a637db8357a2
Gudala <agudala@TX717-16WG8WL.local>
2018-10-18 13:15:18 -0500

    updated

--- LoginApp/App.js
+++ LoginApp/App.js
@@ -1,49 +1,115 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Alert,Button, TouchableOpacity, TextInput, StyleSheet, Text, View} from 'react-native';
+import { GoogleSignin, GoogleSigninButton } from 'react-native-google-signin';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+
+export default class App extends Component {
+componentWillMount() {
+  GoogleSignin.configure( {
+    iosClientId: '956016367956-anhpekqh6me98mku8hs4100vbbcus5d6.apps.googleusercontent.com'
+  })
 }
+  state = {
+    email: '',
+    password: '',
+  };
+  
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+    onLogin() {
+      const { email, password} = this.state;
+  
+      Alert.alert('Alert: Please enter the correct email');
+    }
+    render() {
+      return (
+        <View style={styles.container}>
+          <Text style={styles.welcome}>Welcome</Text>
+          <TextInput style = {styles.input}  
+          value = {this.state.email}
+          keyboardType = 'email-address'
+          onChangeText = {(email) => this.setState({ email })}
+          placeholder = 'Enter email address'
+          placeholderTextColor = 'black'/>
+          
+          <TextInput  style={styles.input}
+          value = {this.state.password}
+          
+          onChangeText = {(password) => this.setState( {password })}
+          placeholder= {'Password'}
+          secureTextEntry={true}
+          placeholderTextColor = 'black'/>
+    
+  <TouchableOpacity style = {styles.buttonStyle}
+  onPress = {() => this.handleSigninGoogle(this.state.email,this.state.password)}>
+  <Text > Sign in with Google + </Text></TouchableOpacity>      
+  <GoogleSigninButton
+    style={{ width: 48, height: 48 }}
+    size={GoogleSigninButton.Size.Icon}
+    color={GoogleSigninButton.Color.Dark}
+    onPress={this._signIn}
+    disabled={this.state.isSigninInProgress} />
+  
+  </View>
+
+      );
+    }
+    handleSigninGoogle(email,password) {
+          GoogleSignin.signIn().then((user) => {
+            console.log(user);
+          }).catch((err) => {
+            console.log('Wrong signin', err);
+          }).done();
+    }
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'white',
+    },
+    welcome: {
+      fontSize: 50,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    input: {
+      width: 200,
+      fontSize: 20,
+      borderColor: 'black',
+      padding: 10,
+      marginVertical: 10,
+      borderWidth: 1,
+  
+    },
+    button: {
+      alignItems: 'center',
+      backgroundColor: 'powderblue',
+      width: 200,
+      height: 44,
+      padding: 10,
+      borderWidth: 1,
+      borderColor: 'black',
+      borderRadius: 25,
+      marginBottom: 10,
+    },
+    buttonText:{
+      fontFamily: 'Baskerville',
+      fontSize: 20,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    buttonStyle: {
+      fontSize: 18,
+      fontWeight: 'bold',
+      color: '#FFF',
+      borderRadius: 5,
+      backgroundColor: 'red',
+      padding: 8
+    }
+  });
+  
+  
--- LoginApp/ios/LoginApp/AppDelegate.m
+++ LoginApp/ios/LoginApp/AppDelegate.m
@@ -6,7 +6,7 @@
  */
 
 #import "AppDelegate.h"
-
+#import "RNGoogleSignin.h"
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
@@ -31,5 +31,10 @@
   [self.window makeKeyAndVisible];
   return YES;
 }
-
+- (BOOL)application:(UIApplication *)application openURL:(nonnull NSURL *)url options:(nonnull NSDictionary<NSString *,id> *)options {
+  return [RNGoogleSignin application:application
+                             openURL:url
+                   sourceApplication:options[UIApplicationOpenURLOptionsSourceApplicationKey]
+                          annotation:options[UIApplicationOpenURLOptionsAnnotationKey]];
+}
 @end
