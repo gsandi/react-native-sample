deabdef26c65a47cc92013fa83750c5b873ae11f
Mudamulagurthy <amudamula@TX717-006HTDG.local>
2018-10-18 15:26:42 -0500

    Integrating Google Api

--- GoogleLogin/App.js
+++ GoogleLogin/App.js
@@ -7,27 +7,57 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View,TouchableOpacity,buttonText} from 'react-native';
+import { GoogleSignin, GoogleSigninButton } from 'react-native-google-signin';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+
+
 
 type Props = {};
 export default class App extends Component<Props> {
+  componentWillMount(){
+    GoogleSignin.configure({
+      iosClientId: '469280814728-vvghgphqsuv5dfo1qpcukv2675glsntk.apps.googleusercontent.com',
+    })
+  }
+   
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+    return(
+      
+      <View style = {styles.container}>
+    <TouchableOpacity style={styles.buttonContainer} 
+                     onPress={() => {this._callGoogle()}}>
+             <Text  style={styles.buttonText}>LOGIN</Text>
+             </TouchableOpacity>
+             <TouchableOpacity style={styles.buttonContainer} 
+                     onPress={() => {this._logoutGoogle()}}>
+             <Text  style={styles.buttonText}>LOGout</Text>
+             </TouchableOpacity>
+             </View>
     );
   }
-}
+  _callGoogle() {
+    GoogleSignin.signIn().then((user) => {
+   
+        alert('hey '+user.email+ ' logged in!')
+      })
+      .catch((err) => {
+        consol.log('wrong',err);
+      })
+      .done();
+     }
+  _logoutGoogle() {
+    GoogleSignin.signOut().then((user) => {
+        this.setState({ user: null })
+        alert('logout') // Remember to remove the user from your app's state as well
+    })
+      .catch ((err) => {
+        console.log('wrong',err);
+      })
+      .done();
+    }
+  }   
+
 
 const styles = StyleSheet.create({
   container: {
@@ -36,6 +66,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
+  buttonContainer:{
+    backgroundColor: '#2980b6',
+    paddingVertical: 15
+},
   welcome: {
     fontSize: 20,
     textAlign: 'center',
@@ -46,4 +80,9 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  buttonText:{
+    color: '#fff',
+    textAlign: 'center',
+    fontWeight: '700'
+}
 });
--- GoogleLogin/ios/GoogleLogin/AppDelegate.m
+++ GoogleLogin/ios/GoogleLogin/AppDelegate.m
@@ -6,24 +6,24 @@
  */
 
 #import "AppDelegate.h"
-
-#import <React/RCTBundleURLProvider.h>
+#import "RNGoogleSignin.h"
 #import <React/RCTRootView.h>
+#import <React/RCTBundleURLProvider.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   NSURL *jsCodeLocation;
-
+  
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
-
+  
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"GoogleLogin"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
-
+  
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
@@ -31,5 +31,15 @@
   [self.window makeKeyAndVisible];
   return YES;
 }
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+  
+  return [RNGoogleSignin application:application
+                             openURL:url
+                   sourceApplication:sourceApplication
+                          annotation:annotation
+          ];
+}
+
 
 @end
